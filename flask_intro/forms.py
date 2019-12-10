768ec746097965670758e17489aed3afe45bd4f3
afolabisunday <afolabisunday31@yahoo.com>
2019-12-10 11:41:33 +0100

    second commit

--- flask_intro/forms.py
+++ flask_intro/forms.py
@@ -1,8 +1,9 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Length, DataRequired, Email, EqualTo, ValidationError
 from flask_intro.models import User
-
+from flask_wtf.file import FileField, FileAllowed
 
 class RegistratForm(FlaskForm):
     fname = StringField("First Name", validators=[DataRequired(), Length(min=2, max=20)])
@@ -34,3 +35,21 @@ class LoginForm(FlaskForm):
 
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateForm(FlaskForm):
+    fname = StringField("First Name", validators=[DataRequired(), Length(min=2, max=20)])
+
+    lname = StringField("Last Name", validators=[DataRequired(), Length(min=2, max=20)])
+
+    Email = StringField("Email Address", validators=[DataRequired(), Email()])
+
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    
+    submit = SubmitField("Update")
+
+    def validate_email(self, Email):
+        if Email.data != current_user.email:
+            user = User.query.filter_by(email=Email.data).first()
+            if user:
+                raise ValidationError('This email has been used before')
--- flask_intro/route.py
+++ flask_intro/route.py
@@ -1,6 +1,13 @@
+##############################################################
+#   intergrity error needs to be handle in the databes when useer try to update or register with an existing email
+##################################################################
+
+import os
+import secrets
+from PIL import Image
 from flask import redirect, render_template, flash, url_for, request
 from flask_intro.models import User, Post
-from flask_intro.forms import RegistratForm, LoginForm
+from flask_intro.forms import RegistratForm, LoginForm, UpdateForm
 from flask_intro import app, bcrypt, db
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -49,8 +56,36 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pic', picture_fn)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+     
 
-@app.route('/account')
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.fname = form.fname.data
+        current_user.lname = form.lname.data
+        current_user.email = form.Email.data
+        db.session.commit()
+        flash('Details Successfully Updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.fname.data = current_user.fname
+        form.lname.data = current_user.lname
+        form.Email.data = current_user.email
+    image_file = url_for('static', filename='profile_pic/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
