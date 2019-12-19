ec4a7f11e54baa57afd767dd7b726e67dd15eebf
afolabisunday <afolabisunday31@yahoo.com>
2019-12-20 00:44:03 +0100

    updated

--- flask_intro/users/forms.py
+++ flask_intro/users/forms.py
@@ -8,23 +8,16 @@ from flask_wtf.file import FileField, FileAllowed
 
 class RegistratForm(FlaskForm):
     fname = StringField("First Name", validators=[DataRequired(), Length(min=2, max=20)])
-
     lname = StringField("Last Name", validators=[DataRequired(), Length(min=2, max=20)])
-
     Email = StringField("Email Address", validators=[DataRequired(), Email()])
-    
-
     password = PasswordField("Password", validators=[DataRequired()])
-
     Confirm_password = PasswordField("Confirm Password",validators=[DataRequired(), EqualTo("password")])
-
     submit = SubmitField("Sign Up Now")
 
     def validate_email(self, Email):
         user = User.query.filter_by(email=Email.data).first()
         if user:
             raise ValidationError('This email has been used before')
-        
 
 
 class LoginForm(FlaskForm):
